ae07050975952e3549a041210912b943a310e661
Case <case@doocom.cn>
2024-09-11 17:20:34 +0800

    feat: 特性: 🚀 style优化

--- packages/hooks/src/pages/home.vue
+++ packages/hooks/src/pages/home.vue
@@ -2,10 +2,10 @@
 </script>
 
 <template>
-	<div class="flex flex-col items-center gap-4 w-full h-full p-4 text-white">
+	<div class="flex flex-col items-center justify-center gap-4 w-full h-full p-4 text-white">
 		<h3>示例</h3>
 		<div class="flex flex-wrap flex-col gap-4 items-center justify-center w-full">
-			<RouterLink class="border border-solid border-yellow-200 p-2" to="/effect-scope">effect-scope</RouterLink>
+			<RouterLink class="border border-solid border-yellow-200 p-2 rounded-sm" to="/effect-scope">effect-scope</RouterLink>
 		</div>
 	</div>
 </template>
